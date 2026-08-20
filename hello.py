1b09c192eb9373008e3b40fc720bc9baeb5e9af2
Praveenvrao <vpraveenrao453@gmail.com>
2026-08-20 17:12:46 +0530

    Day2_Setup

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
+import requests
+
+# Download a web page
+response = requests.get("https://api.github.com")
+print(response.status_code)  # Should print 200
+
 print("Hello, World! - This is my first program in Python")
-print("I am learning Python programming language and today is my second day")
+print("I am learning Python programming language and today is my Second day of learning it.")
