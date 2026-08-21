07050d66935a1714e608d0056b62fc60264a2425
Praveenvrao <vpraveenrao453@gmail.com>
2026-08-21 17:05:59 +0530

    Day3_Basics of Variables & datatypes

---
+++ Day3_Basics.py
@@ -0,0 +1,13 @@
+# Below code is basic code for Variables and data types in Python
+
+integer_variable = 10
+float_variable = 10.5
+name = "John Doe"
+Boolean_variable = True
+
+print("Integer Variable:", integer_variable)
+print("Float Variable:", float_variable)    
+print("String Variable:", name)
+print("Boolean Variable:", Boolean_variable)
+
+print(name +" "+ "is learning Python programming language and today is his third day of learning it.")
