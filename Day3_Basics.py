b46092ff1bab33c7c5ee1ca754397e855c8e02fd
Praveenvrao <vpraveenrao453@gmail.com>
2026-08-22 09:41:58 +0530

    DAY4_Basics1

--- Day3_Basics.py
+++ Day3_Basics.py
@@ -10,4 +10,13 @@ print("Float Variable:", float_variable)
 print("String Variable:", name)
 print("Boolean Variable:", Boolean_variable)
 
-print(name +" "+ "is learning Python programming language and today is his third day of learning it.")
+print(name +" "+ "is learning Python programming language and today is his third day of learning it and my grade is {integer_variable} and my percentage is {float_variable} and my status is {Boolean_variable}".format(integer_variable=integer_variable, float_variable=float_variable, Boolean_variable=Boolean_variable))
+
+text = "Python Programming language"
+print("The length of the text is:", len(text))
+print(text.upper())
+print(text.lower())
+print(text.replace("Python", "Java"))
+print(text.startswith("Python"))
+print(text.endswith("Nothing"))
+print(text.title())
