17d054f2deb54f877686a1631b032429e8be6931
Praveenvrao <vpraveenrao453@gmail.com>
2026-08-23 16:44:17 +0530

    functionwithparameterbasic

--- functionsBasics.py
+++ functionsBasics.py
@@ -10,3 +10,15 @@ def check_weather():
         print("It's a lovely day")
 
 check_weather()
+
+#function with parameters
+
+def check_weather(temp):
+    if temp > 25:
+        print("It's a hot day")
+    elif temp < 10:
+        print("It's cold day")
+    else:
+        print("It's a lovely day")
+
+check_weather(33)
