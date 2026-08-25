ad893d9962457655d37335d0fbb6d524b13ddf55
Praveenvrao <vpraveenrao453@gmail.com>
2026-08-25 16:48:42 +0530

    importbasics

---
+++ importbasics.py
@@ -0,0 +1,9 @@
+import math
+sqrt_25 = math.sqrt(25)
+print(f"The square root of 25 is: {sqrt_25}")
+
+#import random
+import random
+random_number = random.randint(1, 100)
+print(f"The random number between 1-100 is  {random_number}")
+
