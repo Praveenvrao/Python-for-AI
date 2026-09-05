fa9d9f12eeb5243090332fbe6e790e29e530d5ae
Praveenvrao <vpraveenrao453@gmail.com>
2026-09-05 13:22:14 +0530

    forloop&break&continue

--- Loops.py
+++ Loops.py
@@ -1,3 +1,4 @@
+#While loop
 i = 0
 while i<=10:
     print(f"This is While loop line{i}")
@@ -9,3 +10,26 @@ while i<=10:
     i += 1
 
 print(i)
+
+#while loop
+
+data = [12, 34, 56, 78, 90]
+
+i = 0
+n = len(data)
+while i < n:
+    print(data[i])
+    i += 1
+
+#for loop
+
+Data1 = ["Mallik", 32, "John", True, 43.0]
+
+for value in Data1:
+    print(value)
+
+for i in range(16):
+    if i % 3 != 0:
+       # continue
+        break
+    print(i)
