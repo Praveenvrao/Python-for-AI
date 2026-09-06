6c0867135455e94dee526c7aa5da3044bef706f9
Praveenvrao <vpraveenrao453@gmail.com>
2026-09-06 11:46:50 +0530

    recursionfactorialfunction

--- fact_recursion.py
+++ fact_recursion.py
@@ -5,3 +5,12 @@ def fact(num):
     return res
 
 print(fact(5))
+
+#recussion
+def recursion_fact(num):
+    if num == 1:
+        return 1
+    else:
+        return num * recursion_fact(num -1)
+
+print(recursion_fact(12))
